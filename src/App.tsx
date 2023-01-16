e12750b1def563e3be292d46f67ec9b862d70df5
Sankalp100 <sankalpgour123@gmail.com>
2023-01-16 06:54:58 +0530

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import {Routes, Route} from "react-router-dom"
 import { Container } from "react-bootstrap"
-import { Home } from "./pages/home"
+import { Home } from "./pages/Home"
 import { About } from "./pages/About"
 import { Store } from "./pages/Store"
-
+import { Navbar } from "./components/Navbar"
 
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <Container className="mb-4">
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/store" element={<Store/>}/>
       </Routes>
-    </Container>
+    </Container></>
   )
 }
 
